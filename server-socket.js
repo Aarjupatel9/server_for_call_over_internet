d48a3c932f6f3af8596df9588eaa44f77f7129c9
Aarju Patel <aarjupatel922003@gmail.com>
2023-10-23 14:59:14 +0530

    remove

--- server-socket.js
+++ server-socket.js
@@ -8,41 +8,38 @@ var urlencodedparser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json({ limit: "2000kb" }));
 app.use(bodyParser.urlencoded({ limit: "2000kb", extended: true }));
 
-
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 const port = 10101;
 function serverStart() {
-    http.listen(port, function () {
-        console.log("Server-socket listening at port %d", port);
-    });
+  http.listen(port, function () {
+    console.log("Server-socket listening at port %d", port);
+  });
 }
 serverStart();
 
-
-
 io.on("connection", function (socket) {
+  console.log("Client connected:", socket.id);
+  socket.on("connect", (socket) => {
+    console.log("connected , ", socket.socket_id);
+  });
 
-    console.log("Client connected:", socket.id);
-    socket.on("connect", (socket) => {
-        console.log("connected , ", socket.socket_id);
-    })
+  socket.on("init", function () {
+    console.log("event init || start");
+  });
 
-    socket.on("init", function () {
-      console.log("event init || start");
-    });
-  
-    socket.on("cameraFeed", function (imageBytes, desiredWidth, desiredHeight) {
-      console.log(
-        "event cameraFeed ||   desiredWidth*desiredHeight ",
-        desiredWidth,
-        "*",
-        desiredHeight
-      );
-    });
+  socket.on("cameraFeed", function (imageBytes, desiredWidth, desiredHeight) {
+    console.log(
+      "event cameraFeed ||   desiredWidth*desiredHeight ",
+      desiredWidth,
+      "*",
+      desiredHeight
+    );
+  });
 });
 
 app.get("/", (req, res) => {
-    res.send({ Status: 1 });
-})
+  console.log("test request");
+  res.send({ Status: 1 });
+});
